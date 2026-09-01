d3929f87f00c3de8222aae58e7efc2072c3db657
Kirill Saltykov <debased_false2l@icloud.com>
2026-09-01 10:49:23 +0400

    fix: remove unused image processing dependency

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module HookaLoyalty
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
+    # This application does not generate image variants.
+    config.active_storage.variant_processor = :disabled
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
